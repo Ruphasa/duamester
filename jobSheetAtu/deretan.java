fd08b09dc317375fbe32386865ad37a4226abe75
Ruphasa <rfrizqifauzan@gmail.com>
2024-02-16 19:01:42 +0700

    AKHIRNYA BENER JUGA AKUN KUUUU

--- jobSheetAtu/deretan.java
+++ jobSheetAtu/deretan.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class deretan {
     public static void main(String[] args) {
-        String NIM="";
+        String NIM = "";
         int n;
 
         Scanner sc = new Scanner(System.in);
@@ -12,15 +12,15 @@ public class deretan {
         System.out.print("Masukkan NIM : ");
         NIM = sc.nextLine();
         n = Integer.parseInt(NIM.substring(8));
-        if (n<10) {
-            n+=10;
+        if (n < 10) {
+            n += 10;
         }
         for (int i = 1; i <= n; i++) {
-            if (i==6||i==10) {
+            if (i == 6 || i == 10) {
                 continue;
-            }else if (i%2==0) {
-                System.out.print(i+" ");
-            }else{
+            } else if (i % 2 == 0) {
+                System.out.print(i + " ");
+            } else {
                 System.out.print("* ");
             }
         }

e7989bc7e6bde38e038a797da959f00abfc1a42a
Ruphasa <rfrizqifauzan@gmail.com>
2024-02-21 17:06:21 +0700

    Feat : bk2 with Buku24(parameter) & BukuRizqi

--- JobSheetwo/BukuMain24.java
+++ JobSheetwo/BukuMain24.java
@@ -1,9 +1,11 @@
-package JobSheetwo;
+
+
+import JobSheetwo.Buku24;
 
 public class BukuMain24 {
     public static void main(String[] args) {
         Buku24 bk1 = new Buku24();
-        bk1 .judul = "Today Ends Tomorrow Comes";
+        bk1.judul = "Today Ends Tomorrow Comes";
         bk1.pengarang = "Denanda Pratiwi";
         bk1.halaman = 198;
         bk1.stok = 13;
@@ -13,5 +15,9 @@ public class BukuMain24 {
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilkanInformasi();
+
+        Buku24 bk2 = new Buku24("Self Reward", "Maheera Ayesha", 160, 29, 59000);
+        bk2.terjual(11);
+        bk2.tampilkanInformasi();
     }
 }

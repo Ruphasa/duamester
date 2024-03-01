9c8f9112805bc2d53e3918df977c643647925aef
Ruphasa <rfrizqifauzan@gmail.com>
2024-03-01 17:12:38 +0700

    ADD : BangunRuang Class

---
+++ JobSheetree/LatPrak/BangunRuang/bangunRuang.java
@@ -0,0 +1,33 @@
+package JobSheetree.LatPrak;
+
+public class bangunRuang {
+    int sisi;
+    int jari;
+    double tinggi;
+    double miring;
+    int jenisBangunRuang;
+
+    bangunRuang() {
+
+    }
+
+    double hitungLuasPermukaan() {
+        if (jenisBangunRuang == 2) {
+            return 4.0 * 22.0 * jari * jari / 7.0;
+        } else if (jenisBangunRuang == 1) {
+            return sisi * sisi + 4.0 * (sisi * miring / 2.0);
+        } else {
+            return (22.0 * jari * jari / 7.0) + (22.0 * jari / 7.0 *  miring);
+        }
+    }
+
+    double hitungVolume() {
+        if (jenisBangunRuang == 1) {
+            return sisi * sisi * tinggi / 3.0;
+        } else if (jenisBangunRuang == 2) {
+            return 4.0 * 22.0 * jari * jari * jari / 3.0 / 7.0;
+        } else {
+            return 22.0 * jari * jari * tinggi / 3.0 / 7.0;
+        }
+    }
+}

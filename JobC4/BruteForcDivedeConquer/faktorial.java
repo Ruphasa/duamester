a9056d2e482ddcf7ee07b59384fd94ca320fa002
Ruphasa <rfrizqifauzan@gmail.com>
2024-03-15 23:14:55 +0700

    ADD : faktorial class

---
+++ JobC4/BruteForcDivedeConquer/faktorial.java
@@ -0,0 +1,35 @@
+package BruteForcDivedeConquer;
+
+/**
+ * faktorial
+ */
+public class faktorial {
+
+    int nilai;
+
+    int faktorialBF(int n){
+        int fakto = 1;
+        for(int i = 1; i <= n; i++){
+            fakto = fakto* i;
+        }
+        return fakto;
+    }
+
+    int faktorialWhile(int n){
+        int fakto = 1;
+        int i = 1;
+        while(i <= n){
+            fakto *= i;
+            i++;
+        }
+        return fakto;
+    }
+
+    int faktorialDC(int n){
+        if(n == 0 || n == 1){
+            return 1;
+        }else{
+            return n * faktorialDC(n-1);
+        }
+    }
+}
